c3c751456007df71abed4e20e0132ca1c5ba0950
thales santos <thales.infor144@gmail.com>
2024-05-14 16:28:12 -0300

    fix: Corrige a função do HELPER_DATE.now()

--- helpers/date/index.js
+++ helpers/date/index.js
@@ -4,6 +4,10 @@ const moment = require('moment')
 
 /* --- METHODS --- */
 
-function now () {
-  moment.toDate()
+function now() {
+  return moment().format() // Retorna a data e hora atuais no formato especificado pelo Moment.js
+}
+
+module.exports = {
+  now
 }
