c607e31e480b633732ca0b4dee14ffb2037e5d76
thales santos <thales.infor144@gmail.com>
2024-05-15 16:35:11 -0300

    feat: Adiciona uma verificação para verificar se o email é de um servidor do ifce '@ifce.edu.br'

--- helpers/validations/index.js
+++ helpers/validations/index.js
@@ -1,10 +1,11 @@
 /* --- CONSTS --- */
 
 const DOMINIO_ALUNO_IFCE = /@aluno\.ifce\.edu\.br$/
+const DOMINIO_SERVIDOR_IFCE = /@ifce\.edu\.br$/
 
 /* --- METHODS --- */
 
-exports.validateEmailInstitucionalDeAlunoOuTutor = (email) =>{
+exports.validateEmailInstitucionalDeAlunoOuTutor = (email, DOMINIO) =>{
   // Verifica se o email é uma string
   if (typeof email !== 'string') {
     return false
@@ -17,6 +18,25 @@ exports.validateEmailInstitucionalDeAlunoOuTutor = (email) =>{
 
   // Verifica se o email contém o domínio correto
   if (!DOMINIO_ALUNO_IFCE.test(email)) {
+      return false // O email não contém o domínio correto
+  }
+
+  return true // O email é válido
+}
+
+exports.validateEmailInstitucionalServidor = (email) => {
+   // Verifica se o email é uma string
+  if (typeof email !== 'string') {
+    return false
+  }
+
+  // Verifica se o email tem o formato correto
+  if (!email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
+      return false // O email não tem o formato correto
+  }
+
+  // Verifica se o email contém o domínio correto
+  if (!DOMINIO_SERVIDOR_IFCE.test(email)) {
       return false // O email não contém o domínio correto
   }
 
