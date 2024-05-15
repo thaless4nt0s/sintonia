5d5f9200b0fe3b4cff233e7dacdf5e3acd928710
thales santos <thales.infor144@gmail.com>
2024-05-15 16:33:47 -0300

    feat: Adiciona a model de administrador

--- configs/models.js
+++ configs/models.js
@@ -3,6 +3,7 @@
 function registrar (mongoose) {
   require('../models/alunos')(mongoose)
   require('../models/tutores')(mongoose)
+  require('../models/admins')(mongoose)
 }
 
 module.exports = { registrar }
---
+++ models/admins.js
@@ -0,0 +1,50 @@
+/* --- REQUIRES --- */
+
+const mongoose = require('mongoose')
+
+/* --- CONSTS --- */
+
+const HELPER_VALIDATION = require('../helpers/validations')
+const HELPER_DATE = require('../helpers/date')
+
+/* --- METHODS --- */
+
+module.exports = (mongoose) => {
+  const schema = new mongoose.Schema({
+    nome: {
+      type: String,
+      trim: true,
+      required: [true, 'O campo Nome é obrigatório'],
+      maxlength: 100
+    },
+
+    email: {
+      type: String,
+      required: [true, 'O campo Email é obrigatório'],
+      unique: true,
+      trim: true,
+      lowercase: true,
+      index: true,
+      max: 100,
+      validate: [
+        HELPER_VALIDATION.validateEmailInstitucionalServidor,
+        'O campo Email deve ser um email válido'
+      ]
+    },
+
+    senha: {
+      type: String,
+      required: [true, 'O campo Senha é obrigatório'],
+      trim: true,
+      minlength: [8, 'O campo Senha deve ter no mínimo 8 caracteres']
+    },
+
+    dataRegistro: {
+      type: Date,
+      immutable: true,
+      default: function () { return HELPER_DATE.now() }
+    }
+  })
+
+  return mongoose.model('Admins', schema)
+}
