2313055f70b372a4f45005c43af198446beda9d4
thales santos <thales.infor144@gmail.com>
2024-06-05 10:37:51 -0300

    fix: Adiciona um array na variável para que seja enviado somente um Objeto ao inves de um onjeto dentro de um array na rota GET '/alunos/:idAluno'

--- controllers/alunos.js
+++ controllers/alunos.js
@@ -66,7 +66,7 @@ exports.receberPorId = async (req, res, next) => {
   const { idAluno } = req.params
 
   try {
-    const aluno = await REPOSITORY_ALUNOS.receberPorId(idAluno)
+    const [aluno] = await REPOSITORY_ALUNOS.receberPorId(idAluno)
     HELPER_RESPONSE.success(res, aluno)
   } catch (error) {
     next(error)
