214e7e4d4d6b2da693a00dbc0c4758169a7fc755
thales santos <thales.infor144@gmail.com>
2024-06-11 11:13:53 -0300

    tests: Ajusta o padrão de código

--- tests/integration/index/routes/index.spec.js
+++ tests/integration/index/routes/index.spec.js
@@ -4,6 +4,8 @@
 
 const HELPER_INDEX = require('../../../helpers/')
 
+/* ---- METHODS ---- */
+
 describe('Teste da rota GET "/" ', () => {
   jest.setTimeout(10000)
   test('Deve retornar status 200 "ok"', async () => {
